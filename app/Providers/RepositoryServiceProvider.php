65c0eedb90fdd600e3e773157b3468ed7ad560d0
sahab uddin <sahabuddinriyaj984@gmail.com>
2019-10-17 18:53:01 +0600

    Laravel E-commerce Project added Settings,Brand,Attribute,product

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+ 
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Contracts\CategoryContract;
+use App\Repositories\CategoryRepository;
+use App\Contracts\AttributeContract;
+use App\Repositories\AttributeRepository;
+use App\Contracts\BrandContract;
+use App\Repositories\BrandRepository;
+class RepositoryServiceProvider extends ServiceProvider
+{
+    protected $repositories = [
+        CategoryContract::class         =>          CategoryRepository::class,
+        AttributeContract::class        =>          AttributeRepository::class,
+        BrandContract::class            =>          BrandRepository::class,
+    ];
+ 
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        foreach ($this->repositories as $interface => $implementation)
+        {
+            $this->app->bind($interface, $implementation);
+        }
+    }
+}
